14c0c0ac6fdc944ceee2efba7fb8d017ef49bc88
Gurpartap Dhoot <gd51731@PruCapODC.local>
2021-06-09 02:40:40 +0530

    commit

--- udacity-c3-restapi-feed/src/config/config.ts
+++ udacity-c3-restapi-feed/src/config/config.ts
@@ -1,24 +1,27 @@
 export const config = {
   "dev": {
-    'username': "udagramprojectgd",
-    'password': "udagramprojectgd",
-    'database': "udagramprojectgd",
-    'host': "udagramprojectgd.czwnd9cqqfoh.us-east-2.rds.amazonaws.com",
-    'dialect': 'postgres',
-    'aws_region': "us-east-2",
-    'aws_profile': "udagramprojectgd",
-    'aws_media_bucket': "project-bucket-gd",
-    'url': "http://localhost:8080",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host":process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket":process.env.AWS_MEDIA_BUCKET,
+    "url": process.env.URL
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagramprojectgd",
-    "host": "",
-    "dialect": "postgres"
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host":process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket":process.env.AWS_MEDIA_BUCKET,
+    "url": process.env.URL
   },
   "jwt":{
-    secret:"helloworld"
-
+    "secret": process.env.JWT_SECRET
   }
 }
