ea56349f78ccde7f402c7e54b4f232a112908b3d
Gurpartap Dhoot <gd51731@PruCapODC.local>
2021-06-08 15:58:25 +0530

    commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramprojectgd    
+export POSTGRES_PASSWORD=udagramprojectgd
+export POSTGRES_HOST=udagramprojectgd.czwnd9cqqfoh.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=udagramprojectgd
+export AWS_BUCKET=project-bucket-gd
+export AWS_REGION=us-east-2
+export AWS_PROFILE=udagramprojectgd
+export JWT_SECRET=hello
+export URL=http://localhost:8100
